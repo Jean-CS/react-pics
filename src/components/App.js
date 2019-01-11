6c41b024ab3393b7f587685ff05dcd0f62eba5fe
Jean Santos <jeansantosc@gmail.com>
2019-01-10 22:20:43 -0200

    making api request with axios and async/await

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import SearchBar from './SearchBar';
 
-class App extends Component {
-    onSearchSubmit(term) {
+const apiUrl = 'https://api.unsplash.com/search/photos';
+const accessKey = 'ab7b90693ac78426bb919a3981e14b67de4b4db887cf1472fedb47c8676f1cea';
 
+class App extends Component {
+    async onSearchSubmit(term) {
+        const response = await axios.get(apiUrl, {
+            headers: {
+                Authorization: 'Client-ID ' + accessKey,
+            },
+            params: { query: term },
+        });
     }
 
     render() {
