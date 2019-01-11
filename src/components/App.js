e688d1816a51beb20e797c5cd7ad2439d3e3f5d0
Jean Santos <jeansantosc@gmail.com>
2019-01-10 22:31:28 -0200

    setting state and fixing callback scope/context with async arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -7,19 +7,24 @@ const apiUrl = 'https://api.unsplash.com/search/photos';
 const accessKey = 'ab7b90693ac78426bb919a3981e14b67de4b4db887cf1472fedb47c8676f1cea';
 
 class App extends Component {
-    async onSearchSubmit(term) {
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
         const response = await axios.get(apiUrl, {
             headers: {
                 Authorization: 'Client-ID ' + accessKey,
             },
             params: { query: term },
         });
+
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className='ui container' style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
